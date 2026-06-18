a001fd7abebfe8abaaeaa95a4e2125f7022998c6
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-06-18 11:25:33 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,24 +1,44 @@
-export default async function handler(req, res) {
-
+export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
 
-  try {
+  const data = {
+    products: [
+      {
+        id: 1,
+        name: "Nike Air",
+        price: 2500,
+        color: "أبيض",
+        size: 42,
+        qty: 10,
+        image: "nassim01.png"
+      },
+      {
+        id: 2,
+        name: "Nike Air",
+        price: 2600,
+        color: "أبيض",
+        size: 43,
+        qty: 5,
+        image: "nassim02.png"
+      },
+      {
+        id: 3,
+        name: "Nike Air",
+        price: 2700,
+        color: "أسود",
+        size: 42,
+        qty: 8,
+        image: "nassim03.png"
+      }
+    ],
 
-    const GOOGLE_URL = process.env.GOOGLE_SCRIPT_URL;
+    delivery: [
+      { wilaya: "الجزائر", home: 400, office: 250 },
+      { wilaya: "البليدة", home: 350, office: 200 },
+      { wilaya: "وهران", home: 500, office: 300 }
+    ]
+  };
 
-    const response = await fetch(GOOGLE_URL);
-
-    const data = await response.json();
-
-    return res.status(200).json(data);
-
-  } catch (err) {
-
-    return res.status(500).json({
-      ok: false,
-      error: err.message
-    });
-
-  }
-
+  res.status(200).json(data);
 }
