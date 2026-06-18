7b15aacf19bd2261f77d94b094f0b1e9f4282995
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-06-18 17:37:28 +0100

    Update delivery.js

--- api/delivery.js
+++ api/delivery.js
@@ -2,9 +2,7 @@ export default async function handler(req,res){
 
   res.setHeader("Access-Control-Allow-Origin","*");
 
-  const url = process.env.GOOGLE_SCRIPT_URL + "?action=delivery";
-
-  const r = await fetch(url);
+  const r = await fetch(process.env.GOOGLE_SCRIPT_URL + "?action=delivery");
   const data = await r.json();
 
   res.status(200).json(data);
