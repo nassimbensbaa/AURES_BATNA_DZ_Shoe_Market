740d0c18734ce55df7c8d668a9d356225f991bb8
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-06-18 09:58:55 +0100

    Add files via upload

---
+++ api/send.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if(req.method === "OPTIONS") return res.status(200).end();
+  if(req.method !== "POST") return res.status(405).json({ ok:false });
+
+  try {
+
+    const GOOGLE_URL = process.env.GOOGLE_SCRIPT_URL;
+
+    const response = await fetch(GOOGLE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body)
+    });
+
+    const text = await response.text();
+
+    return res.status(200).json({
+      ok: true,
+      result: text
+    });
+
+  } catch(err){
+    return res.status(500).json({
+      ok: false,
+      error: err.message
+    });
+  }
+}
