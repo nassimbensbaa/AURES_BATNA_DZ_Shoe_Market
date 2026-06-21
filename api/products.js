3704eb633df8f8c2de6c4207976bd0e365a67ae2
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-06-21 08:42:21 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,9 +1,26 @@
-export default async function handler(req,res){
+export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const r = await fetch(process.env.GOOGLE_SCRIPT_URL + "?action=products");
-  const data = await r.json();
+  try {
 
-  res.status(200).json(data);
+    const GOOGLE_URL = process.env.GOOGLE_SCRIPT_URL;
+
+    const response = await fetch(
+      GOOGLE_URL + "?action=products"
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (err) {
+
+    res.status(500).json({
+      ok: false,
+      error: err.message
+    });
+
+  }
+
 }
